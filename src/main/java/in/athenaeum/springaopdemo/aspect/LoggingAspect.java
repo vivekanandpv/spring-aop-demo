1aa9510f9db86a0faed0bd0c9ff9ce9a3945b2b8
vivek <vivek@athenaeum.in>
2021-05-27 07:59:50 +0530

    done

--- src/main/java/in/athenaeum/springaopdemo/aspect/LoggingAspect.java
+++ src/main/java/in/athenaeum/springaopdemo/aspect/LoggingAspect.java
@@ -11,14 +11,17 @@ import java.util.logging.Logger;
 @Aspect
 @EnableAspectJAutoProxy
 @Component
-public class LoggingAspect {
+public class LoggingAspect {    //  Cross-cutting concern
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
+    //  Before is an advice
+    //  execution... is a join point and uses point-cut expression
     @Before("execution(* in.athenaeum.springaopdemo.controller.SampleController.greeter())")
     public void beforeExecution() {
         logger.info("About to execute the method");
     }
 
+    //  After is an advice
     @After("execution(* in.athenaeum.springaopdemo.controller.SampleController.greeter())")
     public void afterExecution() {
         logger.info("Finished executing the method");
