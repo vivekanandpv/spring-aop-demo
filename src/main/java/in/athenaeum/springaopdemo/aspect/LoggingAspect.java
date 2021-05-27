79d7f0c516752125b197be70c2db30ad45b555bd
vivek <vivek@athenaeum.in>
2021-05-27 08:09:07 +0530

    done

--- src/main/java/in/athenaeum/springaopdemo/aspect/LoggingAspect.java
+++ src/main/java/in/athenaeum/springaopdemo/aspect/LoggingAspect.java
@@ -1,6 +1,8 @@
 package in.athenaeum.springaopdemo.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -14,16 +16,20 @@ import java.util.logging.Logger;
 public class LoggingAspect {    //  Cross-cutting concern
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
-    //  Before is an advice
-    //  execution... is a join point and uses point-cut expression
-    @Before("execution(* in.athenaeum.springaopdemo.controller.SampleController.greeter())")
-    public void beforeExecution() {
-        logger.info("About to execute the method");
-    }
+    @Around("execution(* in.athenaeum.springaopdemo.controller.SampleController.greeter(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        //  get the arguments
+        Object[] args = joinPoint.getArgs();
 
-    //  After is an advice
-    @After("execution(* in.athenaeum.springaopdemo.controller.SampleController.greeter())")
-    public void afterExecution() {
-        logger.info("Finished executing the method");
+        logger.info("greeter() called with: ");
+        for (Object a: args) {
+            logger.info("Argument: " + a);
+        }
+
+        Object greeterReturnValue = joinPoint.proceed();
+
+        logger.info("greeter() finished with result: " + greeterReturnValue);
+
+        return greeterReturnValue;
     }
 }
--- src/main/java/in/athenaeum/springaopdemo/controller/SampleController.java
+++ src/main/java/in/athenaeum/springaopdemo/controller/SampleController.java
@@ -1,14 +1,15 @@
 package in.athenaeum.springaopdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/sample")
 public class SampleController {
-    @GetMapping
-    public String greeter() {
-        return "Hi there!";
+    @GetMapping("/{firstName}/{lastName}")
+    public String greeter(@PathVariable String firstName, @PathVariable String lastName) {
+        return "Hi there! " + firstName + " " + lastName + "!";
     }
 }
